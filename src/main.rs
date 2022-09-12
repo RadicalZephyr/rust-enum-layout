e18c1da565ac60311b90100aa50a43c531c362bf
Zefira Shannon <earthlingzephyr@gmail.com>
2022-09-12 04:34:47 -0700

    Show that enum new types (single variant) don't add size

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,22 @@ enum R3 {
     C,
 }
 
+enum Comp1R0 {
+    A(R0),
+}
+
+enum Comp1R1 {
+    A(R1),
+}
+
+enum Comp1R2 {
+    A(R2),
+}
+
+enum Comp1R3 {
+    A(R3),
+}
+
 macro_rules! print_sizes {
     { $( $ty_name:ty ),* } => {
         $(
@@ -29,4 +45,5 @@ macro_rules! print_sizes {
 
 pub fn main() {
     print_sizes!(R0, R1, R2, R3);
+    print_sizes!(Comp1R0, Comp1R1, Comp1R2, Comp1R3);
 }
