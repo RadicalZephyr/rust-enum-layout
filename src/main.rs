9caf8ac9691e0fb891b53a33b64d741f44cf19f8
Zefira Shannon <earthlingzephyr@gmail.com>
2022-09-12 04:41:38 -0700

    Show sizes of some composite enums

--- src/main.rs
+++ src/main.rs
@@ -35,8 +35,24 @@ enum Comp1R3 {
     A(R3),
 }
 
+enum Comp2R0 {
+    A(R0),
+    B(R0),
+}
+
+enum Comp2R1 {
+    A(R1),
+    B(R1),
+}
+
+enum Comp2R0R1 {
+    A(R0),
+    B(R1),
+}
+
 macro_rules! print_sizes {
-    { $( $ty_name:ty ),* } => {
+    { $heading:literal, $( $ty_name:ty ),* $(,)? } => {
+        println!(concat!("\n", $heading));
         $(
             println!("size of {}: {} bytes", stringify!($ty_name), size_of::<$ty_name>());
         )*
@@ -44,6 +60,20 @@ macro_rules! print_sizes {
 }
 
 pub fn main() {
-    print_sizes!(R0, R1, R2, R3);
-    print_sizes!(Comp1R0, Comp1R1, Comp1R2, Comp1R3);
+    print_sizes!("Simple enums (no nested types)", R0, R1, R2, R3);
+
+    print_sizes!(
+        "Composite new-type enums (single variant)",
+        Comp1R0,
+        Comp1R1,
+        Comp1R2,
+        Comp1R3,
+    );
+
+    print_sizes!(
+        "Composite enums (multiple variants)",
+        Comp2R0,
+        Comp2R1,
+        Comp2R0R1,
+    );
 }
